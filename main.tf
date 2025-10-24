ce85244091c46640c6ccf35e26770357049d2fe2
Anton <patsev.anton@gmail.com>
2025-10-24 18:11:40 +0600

    add compute.admin

--- main.tf
+++ main.tf
@@ -90,3 +90,15 @@ resource "yandex_resourcemanager_folder_iam_member" "vm_sa_editor" {
   role      = "editor"
   member    = "serviceAccount:${yandex_iam_service_account.vm_sa.id}"
 }
+
+resource "yandex_resourcemanager_folder_iam_member" "vm_sa_compute_admin" {
+  folder_id = coalesce(var.folder_id, data.yandex_client_config.client.folder_id)
+  role      = "compute.admin"
+  member    = "serviceAccount:${yandex_iam_service_account.vm_sa.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "vm_sa_vpc_user" {
+  folder_id = coalesce(var.folder_id, data.yandex_client_config.client.folder_id)
+  role      = "vpc.user"
+  member    = "serviceAccount:${yandex_iam_service_account.vm_sa.id}"
+}
